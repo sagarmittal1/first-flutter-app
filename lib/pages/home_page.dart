6a097f8a1dff6420e2e12a5f8ad5ab20765a87f2
Sagar Mittal <mittalsagar006@gmail.com>
2022-04-22 03:28:07 +0530

    JSON mapping and update to show all products

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'dart:convert';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   int days = 10;
+
   String name = "Anshika Shukla";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData () async {
+    // await Future.delayed(Duration(seconds: 2));
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+    // print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +41,14 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty) ? ListView.builder(
           itemCount: CatalogModel.items.length,
           itemBuilder: (context, index) {
             return ItemWidget(
               item: CatalogModel.items[index],
             );
           },
-        ),
+        ) : Center(child: CircularProgressIndicator(),),
       ),
       drawer: MyDrawer(),
     );
